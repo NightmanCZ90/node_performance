7566337cece6045ae0ba6c9b975ea3c150e1b8f0
NightmanCZ90 <novak.jan90@gmail.com>
2022-05-16 23:38:35 +0200

    feat: Create a node cluster

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-app.get('/', (req, res) => {
-  res.send('Hi there');
-});
-
-app.listen(3000);
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode
+  cluster.fork();
+} else {
+  // Im a child, Im going to act like a server and do nothing else
+  const express = require('express');
+  const app = express();
+  
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+  }
+  
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+  
+  app.listen(3000);
+}
