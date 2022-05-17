1a9b8a067d08634a577e6f19a122e423b55e42e8
NightmanCZ90 <novak.jan90@gmail.com>
2022-05-17 21:28:24 +0200

    refactor: Remove cluster

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const cluster = require('cluster');
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but in child mode
-  cluster.fork();
-} else {
-  // Im a child, Im going to act like a server and do nothing else
-  const express = require('express');
-  const app = express();
-  
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
-  }
-  
-  app.get('/', (req, res) => {
-    doWork(5000);
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     res.send('Hi there');
   });
-  
-  app.listen(3000);
-}
+});
+
+app.get('/fast', (req, res) => {
+  res.send('This was fast!');
+});
+
+app.listen(3000);
